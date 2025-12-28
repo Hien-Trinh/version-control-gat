5c60464901de8d8ecad264fee9af5e87eea2af0c
Hien-Trinh <hientrinhminh32@gmail.com>
2025-12-28 14:46:20 -0500

    added class to handle gat_object

---
+++ include/object.h
@@ -0,0 +1,36 @@
+/*
+Blueprint for gat objects and how the code should handle them in memory
+*/
+
+#include <stddef.h>
+
+typedef enum {
+  OBJ_COMMIT,
+  OBJ_TREE,
+  OBJ_BLOB,
+  OBJ_TAG
+} object_type;
+
+typedef struct {
+  object_type type;
+  size_t size;
+  void* data;     // Generic pointer (could be Tree or Blob object)
+  char hash[41];  // 40 chars of SHA-1 + 1 terminator
+} gat_object;
+
+/*
+How its used:
+1. Create the struct
+git_object obj;
+
+2. Set the type
+obj.type = OBJ_BLOB;
+
+3. Load data
+obj.data = read_file("test.txt", &obj.size);
+
+4. Calculate Hash
+sha1_hash(obj.data, obj.size, obj.hash);
+
+Now 'obj' contains everything needed to write to the .gat folder
+*/
